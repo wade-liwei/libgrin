4b5ebb25c59283627288a9276c74fe876703a4b4
Wade <liwei@biconomy.com>
2020-02-20 14:03:28 +0800

    add url parameter

--- example/owner_api.go
+++ example/owner_api.go
@@ -25,10 +25,10 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var url = "http://127.0.0.1:3420/v2/owner"
+//var url = "http://127.0.0.1:3420/v2/owner"
 
 // RetrieveOutputs returns a list of outputs from the active account in the
-func RetrieveOutputs(includeSpent, refreshFromNode bool, txID *uint32) (bool, *[]libwallet.OutputCommitMapping, error) {
+func RetrieveOutputs(url string, includeSpent, refreshFromNode bool, txID *uint32) (bool, *[]libwallet.OutputCommitMapping, error) {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{includeSpent, refreshFromNode, txID}
 	paramsBytes, err := json.Marshal(params)
@@ -77,7 +77,7 @@ func RetrieveOutputs(includeSpent, refreshFromNode bool, txID *uint32) (bool, *[
 }
 
 // RetrieveTxs returns a list of Transaction Log Entries from the active account in the
-func RetrieveTxs(refreshFromNode bool, txID *uint32, txSlateID *uuid.UUID) (bool, *[]libwallet.TxLogEntry, error) {
+func RetrieveTxs(url string, refreshFromNode bool, txID *uint32, txSlateID *uuid.UUID) (bool, *[]libwallet.TxLogEntry, error) {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{refreshFromNode, txID, txSlateID}
 	paramsBytes, err := json.Marshal(params)
@@ -126,7 +126,7 @@ func RetrieveTxs(refreshFromNode bool, txID *uint32, txSlateID *uuid.UUID) (bool
 }
 
 // RetrieveSummaryInfo returns summary information from the active account in the
-func RetrieveSummaryInfo(refreshFromNode bool, minimumConfirmations uint64) (bool, *libwallet.WalletInfo, error) {
+func RetrieveSummaryInfo(url string, refreshFromNode bool, minimumConfirmations uint64) (bool, *libwallet.WalletInfo, error) {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{refreshFromNode, minimumConfirmations}
 	paramsBytes, err := json.Marshal(params)
@@ -180,7 +180,7 @@ type initSendTxArgs struct {
 // InitSendTx initiates a new transaction as the sender, creating a new Slate
 // object containing the sender's inputs, change outputs, and public signature
 // data.
-func InitSendTx(initTxArgs libwallet.InitTxArgs) (*libwallet.Slate, error) {
+func InitSendTx(url string, initTxArgs libwallet.InitTxArgs) (*libwallet.Slate, error) {
 	initSendTxArgs := initSendTxArgs{initTxArgs}
 	client := RPCHTTPClient{URL: url}
 	paramsBytes, err := json.Marshal(initSendTxArgs)
@@ -218,7 +218,7 @@ func InitSendTx(initTxArgs libwallet.InitTxArgs) (*libwallet.Slate, error) {
 
 // TxLockOutputs locks the outputs associated with the inputs to the transaction
 // in the given Slate, making them unavailable for use in further transactions.
-func TxLockOutputs(slate *libwallet.Slate, participantID uint) error {
+func TxLockOutputs(url string, slate *libwallet.Slate, participantID uint) error {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{slate, participantID}
 	paramsBytes, err := json.Marshal(params)
@@ -250,7 +250,7 @@ func TxLockOutputs(slate *libwallet.Slate, participantID uint) error {
 }
 
 // FinalizeTx finalizes a transaction, after all parties have filled in both rounds of Slate generation.
-func FinalizeTx(slateIn libwallet.Slate) (*libwallet.Slate, error) {
+func FinalizeTx(url string, slateIn libwallet.Slate) (*libwallet.Slate, error) {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{slateIn}
 	paramsBytes, err := json.Marshal(params)
@@ -288,7 +288,7 @@ func FinalizeTx(slateIn libwallet.Slate) (*libwallet.Slate, error) {
 
 // PostTx posts a completed transaction to the listening node for validation and
 // inclusion in a block for mining.
-func PostTx(tx core.Transaction, fluff bool) error {
+func PostTx(url string, tx core.Transaction, fluff bool) error {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{tx, fluff}
 	paramsBytes, err := json.Marshal(params)
@@ -320,7 +320,7 @@ func PostTx(tx core.Transaction, fluff bool) error {
 }
 
 // CancelTx cancels a transaction.
-func CancelTx(txID *uint32, txSlateID *uuid.UUID) error {
+func CancelTx(url string, txID *uint32, txSlateID *uuid.UUID) error {
 	client := RPCHTTPClient{URL: url}
 	params := []interface{}{txID, txSlateID}
 	paramsBytes, err := json.Marshal(params)
@@ -352,7 +352,7 @@ func CancelTx(txID *uint32, txSlateID *uuid.UUID) error {
 }
 
 // NodeHeight retrieves the last known height known by the node.
-func NodeHeight() (*libwallet.NodeHeightResult, error) {
+func NodeHeight(url string) (*libwallet.NodeHeightResult, error) {
 	client := RPCHTTPClient{URL: url}
 	envl, err := client.Request("node_height", nil)
 	if err != nil {
